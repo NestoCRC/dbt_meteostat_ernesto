6cce20e617f1bc0c5187b3a79cde7708370b2120
NestoCRC <ernesto.campos@gmail.com>
2026-02-13 16:13:02 +0100

    staging__categories. dropped image column

---
+++ models/northwind_project/staging__categories.sql
@@ -0,0 +1,8 @@
+WITH category_raw AS (
+SELECT *
+FROM {{source('northwind_data', 'categories')}};
+)
+Select categoryid AS category_id,
+       categoryname AS category_name,
+       description
+FROM category_raw
