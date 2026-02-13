de3cb8f3c4bb3b72f1b1205982bf1b47636891e0
NestoCRC <ernesto.campos@gmail.com>
2026-02-13 14:05:31 +0100

    creation staging_order_details changes to column names orderid as varchar product id as numeric unitprice as numeric dicount as numeric

---
+++ models/northwind_project/staging__order_details.sql
@@ -0,0 +1,9 @@
+WITH staging_order_details AS (
+SELECT *
+FROM {{source('northwind_data' , 'order_details')}}
+SELECT orderid::VARCHAR AS order_id,
+       productid AS product_id,
+       unitprice::NUMERIC AS unit_price,
+       quantity,
+       discount::NUMERIC
+FROM staging_order_details
