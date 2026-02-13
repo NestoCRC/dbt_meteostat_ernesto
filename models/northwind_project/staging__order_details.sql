ba65c3e3d89e46eb3d1388e020526c5b0b36febb
NestoCRC <ernesto.campos@gmail.com>
2026-02-13 14:08:21 +0100

    fixed query missing a )

--- models/northwind_project/staging__order_details.sql
+++ models/northwind_project/staging__order_details.sql
@@ -1,6 +1,7 @@
 WITH staging_order_details AS (
 SELECT *
 FROM {{source('northwind_data' , 'order_details')}}
+)
 SELECT orderid::VARCHAR AS order_id,
        productid AS product_id,
        unitprice::NUMERIC AS unit_price,
