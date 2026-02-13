15cca54fd03a4a654ffe8c91af25d29a3735663d
NestoCRC <ernesto.campos@gmail.com>
2026-02-13 12:48:24 +0100

    staging__orders.sql creation changing order_id to VARCHAR, dates to Date. Column names ammended

---
+++ models/northwind_project/staging__orders.sql
@@ -0,0 +1,13 @@
+SELECT orderid::varchar AS order_id,
+       customerid AS customer_id,
+       employeeid AS employee_id,
+       orderdate::DATE AS order_date,
+       requireddate::DATE AS required_date,
+       shippeddate::DATE AS shipped_date,
+       shipname AS customer_name,
+       shipaddress AS address,
+       shipcity AS city,
+       shipregion AS region, 
+       shippostalcode AS postal_code,
+       shipcountry AS country
+FROM {{source('northwind','orders')}}
